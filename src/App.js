9add2000befd6bb148f001bb85edc08bc26b9874
TJ Chmielewski <hello@tj34.com>
2021-06-04 09:31:04 -0400

    confetti attempt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useRef } from 'react';
 import lottie from 'lottie-web';
 import './App.css';
+import ConfettiExplosion from '@reonomy/react-confetti-explosion';
 
 function App() {
 
   const bolts = useRef(null)
   const bolts2 = useRef(null)
   const star1 = useRef(null)
+  const [isExploding, setIsExploding] = React.useState(true);
 
   useEffect(() => {
       lottie.loadAnimation({
@@ -39,6 +41,12 @@ function App() {
   return (
     <div className="App">
       <h1>Animation Demo</h1>
+      <div className="confetti">
+      {/* tried using this https://www.npmjs.com/package/@reonomy/react-confetti-explosion */}
+        <>
+          {isExploding && <ConfettiExplosion />}
+        </>
+      </div>
       <h2>Bolts</h2>
       <div className="boltContainer">
         <div className="bolt2" ref={bolts2}></div>
@@ -49,6 +57,7 @@ function App() {
         <h2>Stars</h2>
         <div className="star1" ref={star1}></div>
       </div>
+
     </div>
   );
 }
