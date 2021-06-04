58d00a57a2e3fc99e720edcbe1270a3e9575bb16
TJ Chmielewski <hello@tj34.com>
2021-06-03 20:20:52 -0400

    bolts animations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
 import './App.css';
 
 function App() {
+
+  const bolts = useRef(null)
+  const bolts2 = useRef(null)
+
+  useEffect(() => {
+      lottie.loadAnimation({
+        container: bolts.current,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        animationData: require('./bolt-1.json')
+      })
+  }, [])
+  useEffect(() => {
+      lottie.loadAnimation({
+        container: bolts2.current,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        animationData: require('./bolt-1.json')
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Animation Demo</h1>
+      <h2>Bolts</h2>
+      <div className="bolt" ref={bolts}></div>
+      <div className="bolt2" ref={bolts2}></div>
+      <h2>Stars</h2>
+      <div>Placeholder</div>
     </div>
   );
 }
